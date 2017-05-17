45100e99b6fc039e277f9911c39adb2e1c22cf01
Hael Chan <f.procumbens@gmail.com>
2017-05-18 00:25:52 +0800

    Update DoctorDao.java

--- experi.dao/DoctorDao.java
+++ experi.dao/DoctorDao.java
@@ -12,7 +12,7 @@ import experi.entity.Doctor;
 
 public class DoctorDao extends BaseDao {
 	/**
-	 *将医生插入数据库
+	 *灏嗗尰鐢熸彃鍏ユ暟鎹簱
 	 */
 	public void insertDoctor(Doctor doctor){
 		getJdbcTemplate();
@@ -24,7 +24,7 @@ public class DoctorDao extends BaseDao {
 		}
 	}
 	/**
-	 * 删除
+	 * 鍒犻櫎
 	 * @param doctor
 	 */
 	public void deleteDoctor(Doctor doctor){
@@ -38,7 +38,7 @@ public class DoctorDao extends BaseDao {
 		
 	}
 	/**
-	 * 查找固定医生
+	 * 鏌ユ壘鍥哄畾鍖荤敓
 	 */
 
 	public Doctor findById(String doc_id){
@@ -94,6 +94,33 @@ public class DoctorDao extends BaseDao {
 		return null;
 		
 	}
+	
+	public Doctor findByName(String doc_name) {
+		getJdbcTemplate();
+		try{
+			String sql="select * from dbo.Doctor where doc_name=?";
+			List query = jdbcTemplate.query(sql, new Object[]{doc_name}, new RowMapper() {
+				@Override
+				public Object mapRow(ResultSet set, int rownum)
+						throws SQLException {
+					Doctor doctor=new Doctor();
+					doctor.setDoctor_id(set.getString("doc_id"));
+					doctor.setDoctor_name(set.getString(doc_name));
+					doctor.setPassword(set.getString("doc_password"));
+					doctor.setMobile(set.getString("doc_mobile"));
+					return doctor;
+				}
+			});
+			if(query.size() == 0) {
+				return null;
+			}
+			return (Doctor) query.get(0);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 	public static void main(String[] args) {
 		DoctorDao dao=new DoctorDao();
 		Doctor doctor = dao.findById("1111");
