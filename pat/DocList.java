a55cc74523801067ec687a3564bdb6ae71131147
Hael Chan <f.procumbens@gmail.com>
2017-06-01 19:29:31 +0800

    Update DocList.java

--- pat/DocList.java
+++ pat/DocList.java
@@ -103,8 +103,15 @@ public class DocList {
 		Label lblMobile = new Label(shell, SWT.NONE);
 		lblMobile.setFont(SWTResourceManager.getFont("Microsoft YaHei UI", 12, SWT.NORMAL));
 		lblMobile.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-		lblMobile.setBounds(312, 260, 160, 34);
+		lblMobile.setBounds(312, 200, 160, 34);
 		lblMobile.setVisible(false);
+		
+		Label lblWorkTime = new Label(shell, SWT.NONE);
+		lblWorkTime.setFont(SWTResourceManager.getFont("Microsoft YaHei UI", 12, SWT.NORMAL));
+		lblWorkTime.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+		lblWorkTime.setBounds(312, 250, 283, 148);
+		lblWorkTime.setVisible(false);
+		
 		
 		Label lblNotFound = new Label(shell, SWT.NONE);
 		lblNotFound.setText("\u672A\u627E\u5230\u5BF9\u5E94\u533B\u751F");
@@ -113,39 +120,32 @@ public class DocList {
 		lblNotFound.setBounds(307, 253, 194, 34);
 		lblNotFound.setVisible(false);
 		
-		Button btnFind = new Button(shell, SWT.NONE);
-		btnFind.addSelectionListener(new SelectionAdapter() {
-			
-			
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				
-				//PatientDao patientDao = new PatientDao();
-				//Doctor doctor = patientDao.findDoc(patient);
-				doctorDao = new DoctorDao();
-				doctor = doctorDao.findByName(text_toFind.getText());
-				if (doctor == null) {
-					lblNotFound.setVisible(true);
-					lblName.setVisible(false);
-					lblMobile.setVisible(false);
-				}
-				else {
-					lblNotFound.setVisible(false);
-					lblName.setText(doctor.getDoctor_name());
-					lblName.setVisible(true);
-					lblMobile.setText(doctor.getDoctor_mobile());
-					lblMobile.setVisible(true);
-				}
-			}
-			
-		});
-		btnFind.setBounds(494, 48, 114, 34);
-		btnFind.setText("\u67E5\u627E");
-		
 		Label lblPage = new Label(shell, SWT.NONE);
 		lblPage.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
 		lblPage.setText(Integer.toString(page));
 		lblPage.setBounds(380, 398, 33, 24);
+		lblPage.setVisible(false);
+		
+		Label lbl_Name = new Label(shell, SWT.NONE);
+		lbl_Name.setText("\u59D3    \u540D\uFF1A");
+		lbl_Name.setVisible(false);
+		lbl_Name.setFont(SWTResourceManager.getFont("Microsoft YaHei UI", 12, SWT.NORMAL));
+		lbl_Name.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		lbl_Name.setBounds(196, 152, 114, 34);
+		
+		Label lbl_Mobile = new Label(shell, SWT.NONE);
+		lbl_Mobile.setVisible(false);
+		lbl_Mobile.setText("\u624B    \u673A\uFF1A");
+		lbl_Mobile.setFont(SWTResourceManager.getFont("Microsoft YaHei UI", 12, SWT.NORMAL));
+		lbl_Mobile.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		lbl_Mobile.setBounds(196, 200, 114, 34);
+		
+		Label lbl_Worktime = new Label(shell, SWT.NONE);
+		lbl_Worktime.setVisible(false);
+		lbl_Worktime.setText("\u5DE5\u4F5C\u65F6\u95F4\uFF1A");
+		lbl_Worktime.setFont(SWTResourceManager.getFont("Microsoft YaHei UI", 12, SWT.NORMAL));
+		lbl_Worktime.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		lbl_Worktime.setBounds(196, 250, 114, 34);
 		
 		Button btnPre = new Button(shell, SWT.NONE);
 		btnPre.addSelectionListener(new SelectionAdapter() {
@@ -155,17 +155,47 @@ public class DocList {
 					page--;
 					//DoctorDao doctorDao = new DoctorDao();
 					doctor = doctorDao.findByNameAndIndex(text_toFind.getText(), page);
+					lblWorkTime.setText("");
 					if (doctor == null) {
 						lblNotFound.setVisible(true);
 						lblName.setVisible(false);
+						lbl_Name.setVisible(false);
 						lblMobile.setVisible(false);
+						lbl_Mobile.setVisible(false);
+						lblWorkTime.setVisible(false);
+						lbl_Worktime.setVisible(false);
 					}
 					else {
 						lblNotFound.setVisible(false);
 						lblName.setText(doctor.getDoctor_name());
 						lblName.setVisible(true);
+						lbl_Name.setVisible(true);
 						lblMobile.setText(doctor.getDoctor_mobile());
 						lblMobile.setVisible(true);
+						lbl_Mobile.setVisible(true);
+						if (doctor.getWorkOnMon()) {
+							lblWorkTime.setText("周一\n");
+						}
+						if (doctor.getWorkOnTue()) {
+							lblWorkTime.setText(lblWorkTime.getText() + "周二\n");
+						}
+						if (doctor.getWorkOnWed()) {
+							lblWorkTime.setText(lblWorkTime.getText() + "周三\n");
+						}
+						if (doctor.getWorkOnThu()) {
+							lblWorkTime.setText(lblWorkTime.getText() + "周四\n");
+						}
+						if (doctor.getWorkOnFri()) {
+							lblWorkTime.setText(lblWorkTime.getText() + "周五\n");
+						}
+						if (doctor.getWorkOnSat()) {
+							lblWorkTime.setText(lblWorkTime.getText() + "周六\n");
+						}
+						if (doctor.getWorkOnSun()) {
+							lblWorkTime.setText(lblWorkTime.getText() + "周日\n");
+						}
+						lblWorkTime.setVisible(true);
+						lbl_Worktime.setVisible(true);
 					}
 					lblPage.setText(Integer.toString(page));
 				}
@@ -173,6 +203,7 @@ public class DocList {
 		});
 		btnPre.setBounds(320, 398, 33, 24);
 		btnPre.setText("<");
+		btnPre.setVisible(false);
 		
 		Button btnNext = new Button(shell, SWT.NONE);
 		btnNext.addSelectionListener(new SelectionAdapter() {
@@ -181,23 +212,120 @@ public class DocList {
 				page++;
 				//DoctorDao doctorDao = new DoctorDao();
 				doctor = doctorDao.findByNameAndIndex(text_toFind.getText(), page);
+				lblWorkTime.setText("");
 				if (doctor == null) {
 					lblNotFound.setVisible(true);
 					lblName.setVisible(false);
+					lbl_Name.setVisible(false);
 					lblMobile.setVisible(false);
+					lbl_Mobile.setVisible(false);
+					lblWorkTime.setVisible(false);
+					lbl_Worktime.setVisible(false);
 				}
 				else {
 					lblNotFound.setVisible(false);
 					lblName.setText(doctor.getDoctor_name());
 					lblName.setVisible(true);
+					lbl_Name.setVisible(true);
 					lblMobile.setText(doctor.getDoctor_mobile());
 					lblMobile.setVisible(true);
+					lbl_Mobile.setVisible(true);
+					if (doctor.getWorkOnMon()) {
+						lblWorkTime.setText("周一\n");
+					}
+					if (doctor.getWorkOnTue()) {
+						lblWorkTime.setText(lblWorkTime.getText() + "周二\n");
+					}
+					if (doctor.getWorkOnWed()) {
+						lblWorkTime.setText(lblWorkTime.getText() + "周三\n");
+					}
+					if (doctor.getWorkOnThu()) {
+						lblWorkTime.setText(lblWorkTime.getText() + "周四\n");
+					}
+					if (doctor.getWorkOnFri()) {
+						lblWorkTime.setText(lblWorkTime.getText() + "周五\n");
+					}
+					if (doctor.getWorkOnSat()) {
+						lblWorkTime.setText(lblWorkTime.getText() + "周六\n");
+					}
+					if (doctor.getWorkOnSun()) {
+						lblWorkTime.setText(lblWorkTime.getText() + "周日\n");
+					}
+					lblWorkTime.setVisible(true);
+					lbl_Worktime.setVisible(true);
 				}
 				lblPage.setText(Integer.toString(page));
 			}
 		});
 		btnNext.setText(">");
 		btnNext.setBounds(417, 398, 33, 24);
+		btnNext.setVisible(false);
+		
+		
+		Button btnFind = new Button(shell, SWT.NONE);
+		btnFind.addSelectionListener(new SelectionAdapter() {	
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				
+				//PatientDao patientDao = new PatientDao();
+				//Doctor doctor = patientDao.findDoc(patient);
+				doctorDao = new DoctorDao();
+				doctor = doctorDao.findByNameAndIndex(text_toFind.getText(), 1);
+				page = 1;
+				lblPage.setText(Integer.toString(page));
+				if (doctor == null) {
+					lblNotFound.setVisible(true);
+					lblName.setVisible(false);
+					lbl_Name.setVisible(false);
+					lblMobile.setVisible(false);
+					lbl_Mobile.setVisible(false);
+					lblWorkTime.setVisible(false);
+					lbl_Worktime.setVisible(false);
+				}
+				else {
+					lblPage.setVisible(true);
+					btnPre.setVisible(true);
+					btnNext.setVisible(true);
+					lblNotFound.setVisible(false);
+					lblName.setText(doctor.getDoctor_name());
+					lblName.setVisible(true);
+					lbl_Name.setVisible(true);
+					lblMobile.setText(doctor.getDoctor_mobile());
+					lblMobile.setVisible(true);
+					lbl_Mobile.setVisible(true);
+					lblWorkTime.setText("");
+					if (doctor.getWorkOnMon()) {
+						lblWorkTime.setText("周一\n");
+					}
+					if (doctor.getWorkOnTue()) {
+						lblWorkTime.setText(lblWorkTime.getText() + "周二\n");
+					}
+					if (doctor.getWorkOnWed()) {
+						lblWorkTime.setText(lblWorkTime.getText() + "周三\n");
+					}
+					if (doctor.getWorkOnThu()) {
+						lblWorkTime.setText(lblWorkTime.getText() + "周四\n");
+					}
+					if (doctor.getWorkOnFri()) {
+						lblWorkTime.setText(lblWorkTime.getText() + "周五\n");
+					}
+					if (doctor.getWorkOnSat()) {
+						lblWorkTime.setText(lblWorkTime.getText() + "周六\n");
+					}
+					if (doctor.getWorkOnSun()) {
+						lblWorkTime.setText(lblWorkTime.getText() + "周日\n");
+					}
+					lblWorkTime.setVisible(true);
+					lbl_Worktime.setVisible(true);
+				}
+			}
+			
+		});
+		btnFind.setBounds(494, 48, 114, 34);
+		btnFind.setText("\u67E5\u627E");
+		
+		
 		
 		Button btnLink = new Button(shell, SWT.NONE);
 		btnLink.addSelectionListener(new SelectionAdapter() {
@@ -224,6 +352,8 @@ public class DocList {
 		});
 		btnBack.setBounds(640, 484, 97, 34);
 		btnBack.setText("\u8FD4\u56DE");
+		
+		
 
 	}
 }
