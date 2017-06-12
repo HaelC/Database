31ca8ecfb8b1a5dc80a85976b2e202edf103898f
Hael Chan <f.procumbens@gmail.com>
2017-06-12 16:09:46 +0800

    Update PatSelfInfo.java

--- pat/PatSelfInfo.java
+++ pat/PatSelfInfo.java
@@ -4,41 +4,42 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.wb.swt.SWTResourceManager;
 
-import experi.dao.AppointmentDao;
-import experi.dao.DoctorDao;
 import experi.dao.PatientDao;
-import experi.dao.RateDao;
-import experi.entity.Doctor;
+import experi.dao.PressureDao;
 import experi.entity.Patient;
-import experi.entity.Rate;
-import experi.entity.Appointment;
+import experi.entity.Pressure;
+//import ui.Login;
 
 import java.awt.Toolkit;
 import java.sql.Timestamp;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.MessageBox;
-import org.eclipse.swt.events.FocusEvent;
-import org.eclipse.swt.events.FocusListener;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.widgets.DateTime;
-import org.eclipse.swt.widgets.Scale;
 
-public class PatSelfDoc {
+
+public class PatSelfInfo {
 
 	protected Shell shell;
 	protected Display display;
-	
-	private Text txtAdvice;
-	private Text textMessage;
-	private Text textSendMessage;
-	
-	protected String pat_id;
-	private Text textApmtNote;
+	//private Combo sex;
+	//private Combo age;
+	private Text text_Height;
+	private Text text_Weight;
+	private Text text_Systolic;
+	private Text text_familiarDisease;
+	private Text text_historyDisease;
+	private Text text_Note;
+	private Text text_Diastolic;
+	protected String pat_mobile;
+	protected String pat_name;
+	protected String pat_password;
+	private Text text_Age;
 
 	/**
 	 * Launch the application.
@@ -46,19 +47,27 @@ public class PatSelfDoc {
 	 */
 	public static void main(String[] args) {
 		try {
-			PatSelfDoc window = new PatSelfDoc();
+			PatSelfInfo window = new PatSelfInfo();
 			window.open();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public PatSelfDoc() {
+	public PatSelfInfo() {
 		
 	}
 	
-	public PatSelfDoc(String pat_id) {
-		this.pat_id = pat_id;
+	/*
+	public PatSelfInfo(String pat_mobile) {
+		this.pat_mobile = pat_mobile;
+	}
+	*/
+	
+	public PatSelfInfo(String pat_name, String pat_mobile, String pat_password) {
+		this.pat_name = pat_name;
+		this.pat_mobile = pat_mobile;
+		this.pat_password = pat_password;
 	}
 
 	/**
@@ -80,16 +89,13 @@ public class PatSelfDoc {
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		PatientDao patientDao = new PatientDao();
-		Patient patient = patientDao.findById(pat_id);
-		
-		DoctorDao doctorDao = new DoctorDao();
-		Doctor doctor = doctorDao.findById(patient.getDoc_id());
+		//PatientDao patientDao = new PatientDao();
+		//Patient patient = patientDao.findByMobile(pat_mobile);
 		
 		shell = new Shell();
 		shell.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		shell.setSize(800, 600);
-		shell.setText("\u4E3B\u6CBB\u533B\u751F");
+		shell.setText("\u5B8C\u5584\u4E2A\u4EBA\u4FE1\u606F");
 		
 		int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
 		int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
@@ -97,270 +103,137 @@ public class PatSelfDoc {
 		int y = (screenHeight - 600) / 2;
 		shell.setLocation(x, y);
 		
-		Label lblName = new Label(shell, SWT.NONE);
-		lblName.setFont(SWTResourceManager.getFont("Microsoft YaHei UI", 14, SWT.NORMAL));
-		lblName.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
-		lblName.setBounds(422, 41, 209, 49);
-		lblName.setText("医生:" + doctor.getDoctor_name());
+		Label lbl_Sex = new Label(shell, SWT.NONE);
+		lbl_Sex.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+		lbl_Sex.setBounds(246, 25, 90, 24);
+		lbl_Sex.setText("\u6027        \u522B");
+		
+		Combo sex = new Combo(shell, SWT.READ_ONLY);
+		sex.setBounds(360, 25, 114, 32);
+		sex.add("\u7537");
+		sex.add("\u5973");
+		
+		Label lbl_Age = new Label(shell, SWT.NONE);
+		lbl_Age.setFont(SWTResourceManager.getFont("Microsoft YaHei UI", 9, SWT.NORMAL));
+		lbl_Age.setText("\u5E74        \u9F84");
+		lbl_Age.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+		lbl_Age.setBounds(246, 75, 90, 24);
 		
 		/*
-		Canvas canvasPhoto = new Canvas(shell, SWT.NONE);
-		canvasPhoto.setBounds(442, 180, 142, 200);
-		//Need to learn how to add images.
+		Combo age = new Combo(shell, SWT.READ_ONLY);
+		age.setBounds(360, 75, 114, 32);
+		age.setItems("0-30岁","31-40岁","41-50岁","51-60岁","61-70岁","71-80岁","80岁以上");
 		*/
+		text_Age = new Text(shell, SWT.BORDER);
+		text_Age.setBounds(360, 69, 80, 30);
 		
-		Button btnBack = new Button(shell, SWT.NONE);
-		btnBack.addSelectionListener(new SelectionAdapter() {
+		Label lbl_Height = new Label(shell, SWT.NONE);
+		lbl_Height.setText("\u8EAB        \u9AD8");
+		lbl_Height.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+		lbl_Height.setBounds(246, 125, 90, 24);
+		
+		text_Height = new Text(shell, SWT.BORDER);
+		text_Height.setBounds(360, 125, 80, 30);
+		
+		Label lbl_Weight = new Label(shell, SWT.NONE);
+		lbl_Weight.setText("\u4F53        \u91CD");
+		lbl_Weight.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+		lbl_Weight.setBounds(246, 175, 90, 24);
+		
+		text_Weight = new Text(shell, SWT.BORDER);
+		text_Weight.setBounds(360, 175, 80, 30);
+		
+		Label lbl_BloodPressure = new Label(shell, SWT.NONE);
+		lbl_BloodPressure.setText("\u8840        \u538B");
+		lbl_BloodPressure.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+		lbl_BloodPressure.setBounds(246, 225, 90, 24);
+		
+		text_Systolic = new Text(shell, SWT.BORDER);
+		text_Systolic.setBounds(360, 225, 80, 30);
+		
+		Label lbl_familialDisease = new Label(shell, SWT.NONE);
+		lbl_familialDisease.setText("\u5BB6\u65CF\u75C5\u51B5");
+		lbl_familialDisease.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+		lbl_familialDisease.setBounds(246, 275, 90, 24);
+		
+		text_familiarDisease = new Text(shell, SWT.BORDER);
+		text_familiarDisease.setBounds(360, 275, 231, 32);
+		
+		Label lbl_historyDisease = new Label(shell, SWT.NONE);
+		lbl_historyDisease.setText("\u65E2\u5F80\u75C5\u53F2");
+		lbl_historyDisease.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+		lbl_historyDisease.setBounds(246, 325, 90, 24);
+		
+		text_historyDisease = new Text(shell, SWT.BORDER);
+		text_historyDisease.setBounds(360, 325, 231, 32);
+		
+		Label lbl_remark = new Label(shell, SWT.NONE);
+		lbl_remark.setText("\u5907       \u6CE8");
+		lbl_remark.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+		lbl_remark.setBounds(246, 375, 90, 24);
+		
+		text_Note = new Text(shell, SWT.BORDER);
+		text_Note.setBounds(360, 375, 231, 74);
+		
+		Button button = new Button(shell, SWT.NONE);
+		button.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				display.close();
-				PatMain patMain = new PatMain(patient.getPat_mobile());
-				patMain.open();
+				
+				PatientDao patientDao = new PatientDao();
+				Patient patient = new Patient(null, pat_name, pat_password, pat_mobile, sex.getText(), text_Age.getText(), text_Height.getText(), text_Weight.getText(), text_familiarDisease.getText(), text_historyDisease.getText(), null);
+				try {
+					patientDao.insertPatient(patient); 	//first insert the patient's information to SQL.However we don't know 
+					//Patient patient2 = patientDao.findByMobile(pat_mobile);
+					String pat_id = patientDao.findByMobile(pat_mobile).getPat_id();
+					PressureDao pressureDao = new PressureDao();
+					Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+					Pressure pressure = new Pressure(null, pat_id, text_Systolic.getText(), text_Diastolic.getText(), timestamp.toString());
+					pressureDao.insertPressure(pressure);
+					//Pressure pressure2 = pressureDao.findByTime(timestamp);
+					//Patient pat = new Patient(pat_id, pressureDao.findByTime(timestamp).getPressure_id(), null, null, null);
+					MessageBox messageBox= new MessageBox(shell);
+					messageBox.setMessage("\u6ce8\u518c\u6210\u529f");
+					messageBox.open();
+					display.close();
+					//PatMain.main(null);
+					PatMain main = new PatMain(pat_mobile);
+					main.open();
+				}catch (Exception ex) {
+					// TODO: handle exception
+					ex.printStackTrace();
+				}
 			}
 		});
-		btnBack.setBounds(665, 475, 76, 34);
-		btnBack.setText("\u8FD4\u56DE");
+		button.setBounds(330, 475, 114, 34);
+		button.setText("\u63D0\u4EA4");
 		
-		Label lblPhone = new Label(shell, SWT.NONE);
-		lblPhone.setFont(SWTResourceManager.getFont("Microsoft YaHei UI", 10, SWT.NORMAL));
-		lblPhone.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
-		lblPhone.setBounds(389, 153, 269, 29);
-		lblPhone.setText("\u8054\u7CFB\u7535\u8BDD\uFF1A" + doctor.getDoctor_mobile());
-		lblPhone.setVisible(false);
+		Label lblM = new Label(shell, SWT.NONE);
+		lblM.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+		lblM.setBounds(451, 131, 43, 24);
+		lblM.setText("m");
 		
-		Label lblTencent = new Label(shell, SWT.NONE);
-		lblTencent.setText("QQ/\u5FAE\u4FE1\uFF1A000000000");
-		lblTencent.setFont(SWTResourceManager.getFont("Microsoft YaHei UI", 10, SWT.NORMAL));
-		lblTencent.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
-		lblTencent.setBounds(389, 233, 269, 29);
-		lblTencent.setVisible(false);
+		Label lblKg = new Label(shell, SWT.NONE);
+		lblKg.setText("kg");
+		lblKg.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+		lblKg.setBounds(451, 181, 43, 24);
 		
-		/*
-		Label lblAddress = new Label(shell, SWT.NONE);
-		lblAddress.setText("\u5730\u5740\uFF1A\u6D59\u6C5F\u7701\u676D\u5DDE\u5E02**\u8DEF");
-		lblAddress.setFont(SWTResourceManager.getFont("Microsoft YaHei UI", 10, SWT.NORMAL));
-		lblAddress.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
-		lblAddress.setBounds(389, 304, 269, 29);
-		lblAddress.setVisible(false);
-		*/
+		text_Diastolic = new Text(shell, SWT.BORDER);
+		text_Diastolic.setBounds(563, 225, 80, 30);
 		
-		txtAdvice = new Text(shell, SWT.BORDER);
-		txtAdvice.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
-		txtAdvice.setEditable(false);
-		txtAdvice.setText("\u591A\u559D\u70ED\u6C34w");
-		txtAdvice.setBounds(389, 129, 278, 245);
-		txtAdvice.setVisible(false);
+		Label lblmmhg = new Label(shell, SWT.NONE);
+		lblmmhg.setFont(SWTResourceManager.getFont("Microsoft YaHei UI", 7, SWT.NORMAL));
+		lblmmhg.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+		lblmmhg.setBounds(446, 228, 106, 24);
+		lblmmhg.setText("(\u6536\u7F29\u538B)mmHg");
 		
-		textMessage = new Text(shell, SWT.BORDER);
-		textMessage.setText("\u5386\u53F2\u6D88\u606F");
-		textMessage.setEditable(false);
-		textMessage.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
-		textMessage.setBounds(389, 129, 278, 245);
-		textMessage.setVisible(false);
-		
-		textSendMessage = new Text(shell, SWT.BORDER);
-		textSendMessage.setBounds(387, 397, 215, 57);
-		textSendMessage.setVisible(false);
-		
-		Button btnSend = new Button(shell, SWT.NONE);
-		btnSend.setBounds(608, 397, 59, 57);
-		btnSend.setText("\u53D1\u9001");
-		btnSend.setVisible(false);
-		
-		textApmtNote = new Text(shell, SWT.BORDER);
-		textApmtNote.setText("\u5907\u6CE8");
-		textApmtNote.setForeground(SWTResourceManager.getColor(SWT.COLOR_GRAY));
-		textApmtNote.setBounds(389, 153, 269, 152);
-		textApmtNote.setVisible(false);
-		
-		DateTime dateTime = new DateTime(shell, SWT.BORDER);
-		dateTime.setBounds(389, 350, 135, 33);
-		dateTime.setVisible(false);
-		
-		Button btnSubmit = new Button(shell, SWT.NONE);
-		btnSubmit.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				AppointmentDao appointmentDao = new AppointmentDao();
-				//Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-				//Rate rate = new Rate(pat_id, patient.getDoc_id(), scaleScore.getSelection(), timestamp.toString());
-				//rateDao.insertRate(rate);
-				Appointment appointment = new Appointment(patient.getDoc_id(), pat_id, dateTime.toString(), textApmtNote.getText());
-				appointmentDao.insertAppointment(appointment);
-				MessageBox messageBox = new MessageBox(shell);
-				messageBox.setMessage("预约成功");
-				messageBox.open();
-			}
-		});
-		btnSubmit.setBounds(544, 350, 114, 34);
-		btnSubmit.setText("\u9884\u7EA6");
-		btnSubmit.setVisible(false);
-		
-		Scale scaleScore = new Scale(shell, SWT.NONE);
-		scaleScore.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
-		scaleScore.setBounds(371, 244, 260, 48);
-		scaleScore.setMaximum(10);
-		scaleScore.setMinimum(0);
-		scaleScore.setPageIncrement(1);
-		scaleScore.setVisible(false);
-		
-		Label lblPoint = new Label(shell, SWT.NONE);
-		lblPoint.setFont(SWTResourceManager.getFont("Microsoft YaHei UI", 9, SWT.NORMAL));
-		lblPoint.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
-		lblPoint.setBounds(371, 203, 260, 24);
-		lblPoint.setText("   0  1  2  3  4  5  6  7  8  9  10 ");
-		lblPoint.setVisible(false);
-		
-		Button btnRate = new Button(shell, SWT.NONE);
-		btnRate.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				RateDao rateDao = new RateDao();
-				Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-				Rate rate = new Rate(pat_id, patient.getDoc_id(), scaleScore.getSelection(), timestamp.toString());
-				rateDao.insertRate(rate);
-				MessageBox messageBox = new MessageBox(shell);
-				messageBox.setMessage("评分成功");
-				messageBox.open();
-			}
-		});
-		btnRate.setBounds(653, 255, 76, 34);
-		btnRate.setText("\u8BC4\u5206");
-		btnRate.setVisible(false);
-		
-		Button btnContact = new Button(shell, SWT.NONE);
-		btnContact.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				lblPhone.setVisible(true);
-				lblTencent.setVisible(true);
-				//lblAddress.setVisible(true);
-				txtAdvice.setVisible(false);
-				textMessage.setVisible(false);
-				textSendMessage.setVisible(false);
-				textApmtNote.setVisible(false);
-				dateTime.setVisible(false);
-				btnSend.setVisible(false);
-				btnSubmit.setVisible(false);
-				scaleScore.setVisible(false);
-				lblPoint.setVisible(false);
-				btnRate.setVisible(false);
-			}
-		});
-		btnContact.setBounds(135, 100, 114, 34);
-		btnContact.setText("\u8054\u7CFB\u65B9\u5F0F");
-		
-		Button btnAdvice = new Button(shell, SWT.NONE);
-		btnAdvice.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				lblPhone.setVisible(false);
-				lblTencent.setVisible(false);
-				//lblAddress.setVisible(false);
-				txtAdvice.setVisible(true);
-				textMessage.setVisible(false);
-				textSendMessage.setVisible(false);
-				textApmtNote.setVisible(false);
-				dateTime.setVisible(false);
-				btnSend.setVisible(false);
-				btnSubmit.setVisible(false);
-				scaleScore.setVisible(false);
-				lblPoint.setVisible(false);
-				btnRate.setVisible(false);
-			}
-		});
-		btnAdvice.setBounds(135, 180, 114, 34);
-		btnAdvice.setText("\u67E5\u770B\u5EFA\u8BAE");
-		
-		Button btnMessage = new Button(shell, SWT.NONE);
-		btnMessage.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				lblPhone.setVisible(false);
-				lblTencent.setVisible(false);
-				//lblAddress.setVisible(false);
-				txtAdvice.setVisible(false);
-				textMessage.setVisible(true);
-				textSendMessage.setVisible(true);
-				textApmtNote.setVisible(false);
-				dateTime.setVisible(false);
-				btnSend.setVisible(true);
-				btnSubmit.setVisible(false);
-				scaleScore.setVisible(false);
-				lblPoint.setVisible(false);
-				btnRate.setVisible(false);
-			}
-		});
-		btnMessage.setBounds(135, 260, 114, 34);
-		btnMessage.setText("\u6D88\u606F\u8BB0\u5F55");
-		
-		Button btnAppointment = new Button(shell, SWT.NONE);
-		btnAppointment.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				lblPhone.setVisible(false);
-				lblTencent.setVisible(false);
-				//lblAddress.setVisible(false);
-				txtAdvice.setVisible(false);
-				textMessage.setVisible(false);
-				textSendMessage.setVisible(false);
-				textApmtNote.setVisible(true);
-				textApmtNote.addFocusListener(new FocusListener() {
-					
-					@Override
-					public void focusLost(FocusEvent arg0) {
-						// TODO Auto-generated method stub
-						if(textApmtNote.getText().equals("")) {
-							textApmtNote.setText("备注");
-							textApmtNote.setForeground(SWTResourceManager.getColor(SWT.COLOR_GRAY));
-						}
-					}
-					
-					@Override
-					public void focusGained(FocusEvent arg0) {
-						// TODO Auto-generated method stub
-						textApmtNote.setText("");
-						textApmtNote.setForeground(SWTResourceManager.getColor(SWT.COLOR_BLACK));
-						
-					}
-				});
-				dateTime.setVisible(true);
-				btnSend.setVisible(false);
-				btnSubmit.setVisible(true);
-				scaleScore.setVisible(false);
-				lblPoint.setVisible(false);
-				btnRate.setVisible(false);
-			}
-		});
-		btnAppointment.setBounds(135, 340, 114, 34);
-		btnAppointment.setText("\u8FDB\u884C\u9884\u7EA6");
-		
-		Button btnReview = new Button(shell, SWT.NONE);
-		btnReview.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				lblPhone.setVisible(false);
-				lblTencent.setVisible(false);
-				//lblAddress.setVisible(false);
-				txtAdvice.setVisible(false);
-				textMessage.setVisible(false);
-				textSendMessage.setVisible(false);
-				textApmtNote.setVisible(false);
-				dateTime.setVisible(false);
-				btnSend.setVisible(false);
-				btnSubmit.setVisible(false);
-				scaleScore.setVisible(true);
-				lblPoint.setVisible(true);
-				btnRate.setVisible(true);
-			}
-		});
-		btnReview.setBounds(135, 420, 114, 34);
-		btnReview.setText("\u8BC4\u4EF7\u533B\u751F");
+		Label lblmmhg_1 = new Label(shell, SWT.NONE);
+		lblmmhg_1.setText("(\u8212\u5F20\u538B)mmHg");
+		lblmmhg_1.setFont(SWTResourceManager.getFont("Microsoft YaHei UI", 7, SWT.NORMAL));
+		lblmmhg_1.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+		lblmmhg_1.setBounds(650, 225, 106, 24);
 		
 		
-		
-		
-		
-		
+
 	}
 }
